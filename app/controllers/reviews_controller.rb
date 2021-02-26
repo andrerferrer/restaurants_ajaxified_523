734f605f6613791ae07f327f0ead0ba23c510bce
andre ferrer <andrerferrer@hotmail.com>
2021-02-26 06:30:05 -0300

    Add Reviews#Create to the app

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,19 @@
+class ReviewsController < ApplicationController
+  def create
+    @review = Review.new(strong_params)
+    @restaurant = Restaurant.find(params[:restaurant_id]) # /restaurants/:restaurant_id/reviews
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render 'restaurants/show'
+    end
+  end
+
+  private
+
+  def strong_params
+    params.require(:review)
+          .permit(:content)
+  end
+end
